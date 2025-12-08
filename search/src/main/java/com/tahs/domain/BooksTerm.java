631bcfb314817c64954be34c8dea902c807e0f87
giselabcruz <giselaisabelbelmontecruz@gmail.com>
2025-12-08 11:51:15 +0000

    update: serializable terms

--- search/src/main/java/com/tahs/domain/BooksTerm.java
+++ search/src/main/java/com/tahs/domain/BooksTerm.java
@@ -1,8 +1,9 @@
 package com.tahs.domain;
 
+import java.io.Serializable;
 import java.util.List;
 
-public record BooksTerm (
-    String term,
-    List<String> booksId
-){}
+public record BooksTerm(
+        String term,
+        List<String> booksId) implements Serializable {
+}
