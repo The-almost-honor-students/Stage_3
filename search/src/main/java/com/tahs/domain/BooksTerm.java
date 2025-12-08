6fdca3ad88f7af374e85b6e6819b09b41966a848
giselabcruz <giselaisabelbelmontecruz@gmail.com>
2025-12-08 17:48:13 +0000

    refactor: delete serializable implementation

--- search/src/main/java/com/tahs/domain/BooksTerm.java
+++ search/src/main/java/com/tahs/domain/BooksTerm.java
@@ -1,9 +1,8 @@
 package com.tahs.domain;
 
-import java.io.Serializable;
 import java.util.List;
 
 public record BooksTerm(
         String term,
-        List<String> booksId) implements Serializable {
+        List<String> booksId)  {
 }
